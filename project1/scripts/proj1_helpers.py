fa3bc34c8b63659d674d61bb6f775d83e10240cc
Peter K <peter.krcmar@epfl.ch>
2019-10-18 18:55:34 +0200

    Clean up a little

--- project1/scripts/proj1_helpers.py
+++ project1/scripts/proj1_helpers.py
@@ -10,6 +10,8 @@ def load_csv_data(data_path, sub_sample=False):
     x = np.genfromtxt(data_path, delimiter=",", skip_header=1)
     ids = x[:, 0].astype(np.int)
     input_data = x[:, 2:]
+    
+    
 
     # convert class labels from strings to binary (-1,1)
     yb = np.ones(len(y))
@@ -48,22 +50,30 @@ def create_csv_submission(ids, y_pred, name):
             writer.writerow({'Id':int(r1),'Prediction':int(r2)})
 
             
-def split_data(x, y, ratio, seed=None):
+def split_data(x, y, train_ratio, validation_ratio, seed=0):
     """
-    Split the dataset based on the split ratio. If ratio is 0.8 
-    you will have 80% of your data set dedicated to training 
-    and the rest dedicated to testing
+    Split the dataset into train, validation and train sets based on the ratios.
+    The sum of the ratios should be < 1.
     """
+    if train_ratio + validation_ratio > 1:
+        print('Sum of ratio\'s > 1 !')
+        return
+    
     # set seed
-    if seed:
-        np.random.seed(seed)
+    np.random.seed(seed)
     
     n = y.shape[0]
     indices = np.arange(n)
     np.random.shuffle(indices)
-    limit = min(int(n * ratio), n)
-    train_indices, test_indices = indices[:limit], indices[limit:]
+    limit_1 = int(n * train_ratio)
+    limit_2 = int(n * (train_ratio + validation_ratio))
     
-    train_x, test_x = x[train_indices], x[test_indices]
-    train_y, test_y = y[train_indices], y[test_indices]
-    return train_x, train_y, test_x, test_y
+    train_ids, validation_ids, test_ids = indices[:limit_1], indices[limit_1:limit_2], indices[limit_2:]
+    
+    train_x, validation_x, test_x = x[train_ids], x[validation_ids], x[test_ids]
+    train_y, validation_y, test_y = y[train_ids], y[validation_ids], y[test_ids]
+    return train_x, train_y, validation_x, validation_y, test_x, test_y
+    
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    return np.vander(x, N=degree+1, increasing=True)
