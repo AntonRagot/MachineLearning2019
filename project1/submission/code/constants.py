0c3599d31f6a814c30ec48159ee22f45f3fb9d25
Peter K <peter.krcmar@epfl.ch>
2019-10-26 23:10:31 +0200

    Update weights

--- project1/submission/code/constants.py
+++ project1/submission/code/constants.py
@@ -13,11 +13,11 @@ UNDEF = -999.
 JET_NUM_INDEX = 22
 
 # training constants
-K = 4
+K = 10
 UNWANTED = [[4,5,6,12,22,23,24,25,26,27,28,29],
             [4,5,6,12,22,26,27,28,29],
             [22],
-            [22]]      
-LAMBDAS_TO_TEST = np.logspace(-8, 0, 10)
+            [22]]
+LAMBDAS_TO_TEST = np.logspace(-8, 0, 20)
 LAMBDAS_TO_TEST = np.insert(LAMBDAS_TO_TEST, 0, 0)
-DEGREES_TO_TEST = np.arange(5,11)
+DEGREES_TO_TEST = np.arange(4, 11)
