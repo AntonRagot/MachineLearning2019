a28cd1601729308393be04e2f0ec00628645f2fb
Anton <anton.ragot@epfl.ch>
2019-11-18 16:17:45 +0100

    Adding NN of project 2 - Not working ATM

--- project1/scripts/costs.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Function used to compute the loss."""
-
-def compute_loss(y, tx, w):
-    """Calculates the loss using MSE.
-    """
-    err = y - tx@w # compute error vector
-    return 1.0/(2.0*y.shape[0]) * (err.T @ err)
