29e244d3a22ad5a9e84efad197f2e0d84223e815
zbirobin <zbi.robin@gmail.com>
2019-12-19 12:50:31 +0100

    Add model

--- project2/code/model.py
+++ project2/code/model.py
@@ -1,5 +1,33 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 
+from utils import *
 
+# n-gram range
+LO = 1
+HI = 4
+
+CLEAN_TRAIN_PATH = '../data/clean/train.txt'
+
+def get_requested_model(model="CNN"):
+    if model == "CNN":
+        return generate_model_CNN()
+    elif model == "CNN_GRU":
+        return generate_model_CNN_GRU()
+    elif model == "LR":
+        return pipeline_model(model)
+    elif model == "SVC":
+        return pipeline_model(model)
+    elif model == "TREE":
+        return pipeline_model(model)
+    elif model == "BAYES":
+        return pipeline_model(model)
+    else:
+        raise ValueError("Undefined model")
 
 def generate_model_CNN(vocabulary_size=200, Embedding_size=10, max_length=54):
     model = Sequential()
@@ -16,7 +44,35 @@ def generate_model_CNN(vocabulary_size=200, Embedding_size=10, max_length=54):
     model.compile(loss='binary_crossentropy',optimizer=optimizers.Adam(lr=0.001),metrics=['accuracy'])
     return model
 
+def generate_model_CNN_GRU():
+    raise NotImplementedError()
 
-def get_requested_model(model = "CNN"):
-    if model == "CNN":
-        return generate_model_CNN()
+def pipeline_model(model):
+    if model == "LR":
+        clf = Pipeline([
+            ('tfidf', TfidfVectorizer(tokenizer=tokenize, ngram_range=(LO, HI))),
+            ('classifier', LogisticRegression(random_state=0))
+        ])
+    elif model == "SVC":
+        clf = Pipeline([
+            ('tfidf', TfidfVectorizer(tokenizer=tokenize, ngram_range=(LO, HI))),
+            ('classifier', LinearSVC(random_state=0))
+        ])
+    elif model == "TREE":
+        clf = Pipeline([
+            ('tfidf', TfidfVectorizer(tokenizer=tokenize, ngram_range=(LO, HI))),
+            ('classifier', DecisionTreeClassifier(random_state=0, max_depth=8))
+        ])
+    elif model == "Bayes":
+        clf = Pipeline([
+            ('tfidf', TfidfVectorizer(tokenizer=tokenize, ngram_range=(LO, HI))),
+            ('classifier', MultinomialNB(random_state=0))
+        ])
+    else:
+        raise ValueError("Undefined model")
+    train_tweets, train_labels = load_tweets(CLEAN_TRAIN_PATH, True)
+    clf.fit(train_tweets, train_labels)
+    return clf
+
+def tokenize(tweet):
+    return tweet.split()
